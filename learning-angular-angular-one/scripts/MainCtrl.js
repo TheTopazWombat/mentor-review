a8a70d7c054ae7658e7a8c2aeacd17599d851900
W. Isaac Leavitt <iceman.ike@gmail.com>
2016-10-27 14:49:30 -0600

    Add files via upload

---
+++ learning-angular-angular-one/scripts/MainCtrl.js
@@ -0,0 +1,23 @@
+// scripts/MainCtrl.js
+
+angular
+	.module('avengersApp')
+	.controller('MainCtrl', function($scope, dataService) {
+
+		$scope.getHeroes = function(id)  {// javascript hoisting
+			dataService.avengers(id).then(function(response) {
+				$scope.avengers = response;
+			});
+		};
+
+
+		$scope.comics = [
+			{name: '3-D Man', id: '22300'},
+			{name: 'X-Men', id: '22301'},
+			{name: 'Hulk', id: '22302'},
+			{name: 'New Warrios', id: '22303'}
+
+		];
+
+
+	}); // end
