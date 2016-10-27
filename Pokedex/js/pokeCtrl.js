7eac797f04202986f57e416befbddaf78b85bb2e
W. Isaac Leavitt <iceman.ike@gmail.com>
2016-10-27 14:48:48 -0600

    Add files via upload

---
+++ Pokedex/js/pokeCtrl.js
@@ -0,0 +1,25 @@
+angular.module('pokedex')
+  .controller('pokeCtrl', function ($scope, pokeServ) {
+
+    $scope.defaultHide = true;
+
+
+    $scope.getPokemon = function(num) {
+      num = num.toLowerCase();
+      pokeServ.getPokemon(num).then(function(results) {
+        $scope.pokeData = results;
+        $scope.pokePic = $scope.pokeData.sprites.front_default;
+        $scope.defaultHide = false;
+        var name = $scope.pokeData.name.toUpperCase();
+        $scope.pokeName = name;
+        console.log($scope.pokeName);
+        $scope.pokeData.types[0].type = $scope.pokeType;
+        console.log($scope.pokeType);
+      });
+    };
+
+
+
+
+
+  });
---
+++ Pokedex/js/pokeServ.js
@@ -0,0 +1,15 @@
+angular.module('pokedex')
+  .service('pokeServ', function ($http) {
+    this.getPokemon = function(num) {
+      return $http({
+        method: "GET",
+        url: "http://pokeapi.co/api/v2/pokemon/" + num + "/"
+      }).then(function(response) {
+        console.log(response.data);
+        return response.data;
+      });
+    };
+
+
+
+  });
