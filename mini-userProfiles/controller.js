6192ebbb0885b6cf7555c75679f0a4550bcc1c04
W. Isaac Leavitt <iceman.ike@gmail.com>
2016-10-27 14:50:18 -0600

    Add files via upload

---
+++ mini-userProfiles/controller.js
@@ -0,0 +1,13 @@
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
+
+    $scope.getUsers = function() {
+    	$scope.users = mainService.getUsers();
+    };
+
+    $scope.getUsers();
+
+    $scope.toggleFavorite = function(index) {
+    	mainService.toggleFavorite(index);
+    };
+
+});
